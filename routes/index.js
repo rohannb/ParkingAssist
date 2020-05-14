5e1c6551ee65db7dc96cc6880cbc69c766f2ac0f
Rohan Brahmakshatriya <rbrahmakshatriy@deakin.edu.au>
2020-05-14 13:08:08 +1000

    can fetch data from api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var soda = require('soda-js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log('index_request');
+	res.sendfile('views/index.html');
 });
+
+router.get('/test', function(req, res, next) {
+	var parking = new soda.Consumer('data.melbourne.vic.gov.au');
+
+	parking.query()
+	  .withDataset('vh2v-4nfs')
+	  .limit(5)
+	  .where({ bay_id: '3274' })
+	  .getRows()
+	    .on('success', function(rows) { console.log(rows); })
+	    .on('error', function(error) { console.error(error); });
+
+	  res.render('index', { title: 'Express' });
+});
+
+
+
 
 module.exports = router;
